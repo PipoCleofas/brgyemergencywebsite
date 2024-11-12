be1d31fa4f8dcf5bbc32f392238d069acf62ddc3
PipoCleofas <pipsjacobzxc@gmail.com>
2024-11-12 11:50:48 +0800

    https

--- src/components/ApprovalRight.tsx
+++ src/components/ApprovalRight.tsx
@@ -23,7 +23,7 @@ export default function ApprovalRight() {
     }, [checkAccounts, clients]);
 
     const updateUserStatus = async (status: string, userId: number) => {
-        const url = `http://fearless-growth-production.up.railway.app/user/updateStatusUser/${status}`;
+        const url = `https://fearless-growth-production.up.railway.app/user/updateStatusUser/${status}`;
         console.log('Attempting to update user status at:', url);  
     
         try {
